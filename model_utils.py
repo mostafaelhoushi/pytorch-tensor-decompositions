f3d56d1eb1200a8040c72d98c59a28fdfa79bb94
Mostafa Elhoushi <m.elhoushi@ieee.org>
2019-06-07 16:59:44 -0400

    log during training

--- model_utils.py
+++ model_utils.py
@@ -3,21 +3,49 @@ import time
 
 from torch.autograd import Variable
 
-def train(model, train_data_loader, test_data_loader, optimizer, epochs=10, criterion=torch.nn.CrossEntropyLoss()):
-    model.cuda()
-
+def train(model, train_data_loader, test_data_loader, optimizer, epochs=10, criterion=torch.nn.CrossEntropyLoss(), print_freq=10):
     for i in range(epochs):
         print("Epoch: ", i)
-        for i, (batch, label) in enumerate(train_data_loader):
-            model.train()
-            model.zero_grad()
-            batch = batch.cuda()
-            label = label.cuda()
-            input = Variable(batch)
-            criterion(model(input), Variable(label)).backward()
-            optimizer.step()
+        train_epoch(model, train_data_loader, optimizer, i, criterion)
         test(model, test_data_loader)
     print("Finished fine tuning.")
+
+
+def train_epoch(model, train_data_loader, optimizer, epoch, criterion=torch.nn.CrossEntropyLoss(), print_freq=10):
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(len(train_data_loader), batch_time, data_time, losses, top1,
+                             top5, prefix="Epoch: [{}]".format(epoch))
+
+    model.cuda()
+    model.train()
+    for i, (batch, label) in enumerate(train_data_loader):
+        model.zero_grad()
+        batch = batch.cuda()
+        label = label.cuda()
+        input = Variable(batch)
+        output = model(input)
+        loss = criterion(output, Variable(label))
+
+        # measure accuracy and record loss
+        acc1, acc5 = accuracy(output, label, topk=(1, 5))
+        losses.update(loss.item(), input.size(0))
+        top1.update(acc1[0], input.size(0))
+        top5.update(acc5[0], input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if i % print_freq == 0:
+            progress.print(i)
+
+    return (losses.avg, top1.avg.cpu().numpy(), top5.avg.cpu().numpy(), batch_time.avg)
+
 
 def test(model, test_data_loader, criterion=torch.nn.CrossEntropyLoss(), print_freq=10):
     batch_time = AverageMeter('Time', ':6.3f')
