dc14c7469fda9b9f925e1a624b4302ade7c8932f
Mostafa Elhoushi <m.elhoushi@ieee.org>
2019-06-10 18:19:10 -0400

    choose arbitrary model to decompose from pytorch hub

--- main.py
+++ main.py
@@ -15,10 +15,15 @@ from itertools import chain
 from decompositions import decompose_model, cp_decomposition_conv_layer, tucker_decomposition_conv_layer
 from model_utils import train, test
 
+model_names = torch.hub.list('pytorch/vision', force_reload=True)
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--train", dest="train", action="store_true")
     parser.add_argument("--test", dest="test", action="store_true")
+    parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet50',
+        choices=model_names,
+        help="model architecture: " + str(model_names) + " (default: resnet50)")
     parser.add_argument("--decompose", dest="decompose", action="store_true")
     parser.add_argument("--fine_tune", dest="fine_tune", action="store_true")
     parser.add_argument("--train_path", type = str, default = "train")
@@ -26,10 +31,10 @@ def get_args():
     parser.add_argument("--cp", dest="cp", action="store_true", \
         help="Use cp decomposition. uses tucker by default")
     parser.add_argument("-b", "--batch-size", default=256, type=int,
-                        metavar="N",
-                        help="mini-batch size (default: 256), this is the total "
-                            "batch size of all GPUs on the current node when "
-                            "using Data Parallel or Distributed Data Parallel")
+        metavar="N",
+        help="mini-batch size (default: 256), this is the total "
+            "batch size of all GPUs on the current node when "
+            "using Data Parallel or Distributed Data Parallel")
 
     parser.set_defaults(train=False)
     parser.set_defaults(decompose=False)
@@ -46,7 +51,7 @@ if __name__ == '__main__':
         train_data_loader = dataset.loader(args.train_path, args.batch_size)
         test_data_loader = dataset.test_loader(args.test_path, args.batch_size)
     
-        model = models.vgg16(pretrained=True).cuda() # ModifiedVGG16Model().cuda()
+        model = torch.hub.load('pytorch/vision', args.arch, pretrained=True).cuda() 
         optimizer = optim.SGD(model.classifier.parameters(), lr=0.0001, momentum=0.99)
 
         train(model, train_data_loader, test_data_loader, optimizer, epochs=10)
@@ -58,13 +63,13 @@ if __name__ == '__main__':
         if args.decompose:
             model = torch.load("decomposed_model.pth")
         else:
-            model = models.vgg16(pretrained=True).cuda() # ModifiedVGG16Model().cuda()
+            model = torch.hub.load('pytorch/vision', args.arch, pretrained=True).cuda() 
         
         test(model, test_data_loader)
         torch.save(model, "model.pth")
 
     elif args.decompose:
-        model = models.vgg16(pretrained=True).cuda() # torch.load("model").cuda()
+        model = torch.hub.load('pytorch/vision', args.arch, pretrained=True).cuda() 
         model.eval()
         model.cpu()
         model = decompose_model(model, args.cp)
