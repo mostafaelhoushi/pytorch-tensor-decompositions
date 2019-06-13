0807fa892e2973a4a68d0edec78f6fd21783d233
Mostafa Elhoushi <m.elhoushi@ieee.org>
2019-06-12 20:52:12 -0400

    options load optimizer and model

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ def get_args():
     parser.add_argument("--test", dest="test", action="store_true")
     parser.add_argument("--decompose", dest="decompose", action="store_true")
     parser.add_argument("--fine_tune", dest="fine_tune", action="store_true")
+    parser.add_argument("--load_model", dest="load_model")
+    parser.add_argument("--load_opt", dest="load_opt")
     parser.add_argument("--cp", dest="cp", action="store_true", \
         help="Use cp decomposition. uses tucker by default")
     parser.add_argument("-b", "--batch-size", default=256, type=int,
@@ -48,7 +50,10 @@ if __name__ == '__main__':
     args = get_args()
     tl.set_backend("pytorch")
     
-    model = torch.hub.load('pytorch/vision', args.arch, pretrained=True).cuda() 
+    if args.load_model:
+        model = torch.load(args.load_model)
+    else:
+        model = torch.hub.load('pytorch/vision', args.arch, pretrained=True).cuda() 
     
     train_path = os.path.join(args.data, "train")
     test_path = os.path.join(args.data, "val")
@@ -70,14 +75,18 @@ if __name__ == '__main__':
         for param in model.parameters():
             param.requires_grad = True
         
-        if args.decompose:
-            if args.cp:
-                optimizer = optim.SGD(model.parameters(), lr=0.000001)
-            else:
-                optimizer = optim.SGD(model.parameters(), lr=0.001)
+        if args.load_opt:
+            optimizer = torch.load(args.load_opt)
         else:
-            optimizer = optim.SGD(model.classifier.parameters(), lr=0.0001, momentum=0.99)
+            if args.decompose:
+                if args.cp:
+                    optimizer = optim.SGD(model.parameters(), lr=0.000001)
+                else:
+                    optimizer = optim.SGD(model.parameters(), lr=0.001)
+            else:
+                optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.99)
 
+        torch.save(optimizer, "optimizer.pth")
         train(model, train_data_loader, test_data_loader, optimizer, epochs=10)
         torch.save(model, "trained_model.pth")
                
