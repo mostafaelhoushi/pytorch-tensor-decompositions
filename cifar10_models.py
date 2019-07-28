3c2da6c0f7408d16faa82da5b34013411c2837ee
Mostafa Elhoushi <m.elhoushi@ieee.org>
2019-07-27 22:13:23 -0400

    put all CIFAR10 defined models or weights together and separate from imagenet

--- cifar10_models.py
+++ cifar10_models.py
@@ -28,7 +28,7 @@ import torch.nn.init as init
 
 from torch.autograd import Variable
 
-__all__ = ['ResNet', 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet1202']
+__all__ = ['ResNet', 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet1202', 'vgg11', 'vgg13', 'vgg16']
 
 def _weights_init(m):
     classname = m.__class__.__name__
@@ -135,7 +135,7 @@ def resnet1202():
     return ResNet(BasicBlock, [200, 200, 200])
 
 
-def test(net):
+def test_resnet(net):
     import numpy as np
     total_params = 0
 
@@ -145,9 +145,87 @@ def test(net):
     print("Total layers", len(list(filter(lambda p: p.requires_grad and len(p.data.size())>1, net.parameters()))))
 
 
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 09:39:30 2018
+
+@author: tshzzz
+"""
+
+cfg = {
+       'vgg11':[64,'M',128,'M',256,256,'M',512,512,'M',512,512,'M'],
+       'vgg13':[64,64,'M',128,128,'M',256,256,'M',512,512,'M',512,512,'M'],
+       'vgg16':[64,64,'M',128,128,'M',256,256,256,'M',512,512,512,'M',512,512,512,'M']
+       
+       }
+
+class VGG(nn.Module):
+    
+    def __init__(self,layers,num_class=10):
+        super(VGG,self).__init__()
+        
+        self.img_channel = 3
+        self.layers = layers
+        
+        self.conv = self.make_layers()
+        self.fc = nn.Linear(512, num_class)
+        
+    def make_layers(self):
+        
+        layers = []
+        in_channel = self.img_channel
+        for idx in range(len(self.layers)):
+            if self.layers[idx] == 'M':          
+                layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+            else:
+                layers.append(nn.Conv2d(in_channel,self.layers[idx],kernel_size=3,padding=1))
+                layers.append(nn.BatchNorm2d(self.layers[idx]))
+                layers.append(nn.ReLU())
+                in_channel = self.layers[idx]
+        return nn.Sequential(*layers)
+            
+    
+    def forward(self,x):
+        
+        out = self.conv(x)
+        out = out.view(-1,512)
+        out = self.fc(out)
+        
+        return out
+        
+
+def vgg11():
+    return VGG(cfg['vgg11'],num_class=10)
+
+def vgg13():
+    return VGG(cfg['vgg13'],num_class=10)
+       
+def vgg16():
+    return VGG(cfg['vgg16'],num_class=10)
+
+def test_vgg():
+    import torchvision.models as models
+    model = models.vgg16()
+    count = 0
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            count += 1
+    print(count)
+    a = VGG16()
+    count = 0
+    for m in a.modules():
+        if isinstance(m, nn.Conv2d):
+            count += 1
+    print(count)
+    y = a(torch.randn(3,3,32,32))
+    print(y.size())
+
+
 if __name__ == "__main__":
     for net_name in __all__:
         if net_name.startswith('resnet'):
             print(net_name)
-            test(globals()[net_name]())
+            test_resnet(globals()[net_name]())
             print()
+    
+    test_vgg()
