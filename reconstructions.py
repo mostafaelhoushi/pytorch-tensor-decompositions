14c44c7ffc668c88444ca847b134fd8b07c1aa34
Mostafa Elhoushi <m.elhoushi@ieee.org>
2020-01-08 16:53:06 -0500

    reconstruct layers that were decomposed by tucker-1

--- reconstructions.py
+++ reconstructions.py
@@ -37,31 +37,55 @@ def reconstruct_model(model, cp=False):
                 if cp:
                     raise("cp reconstruction not yet supported")
                 else: # tucker reconstruction
-                    assert(len(conv_layers_list)) == 3
-                    [last_layer, core_layer, first_layer] = conv_layers_list
-                    [last_name, core_name, first_name] = conv_names_list 
+                    if(len(conv_layers_list) == 3):
+                        [last_layer, core_layer, first_layer] = conv_layers_list
+                        [last_name, core_name, first_name] = conv_names_list 
+                        print("BEFORE last_weight: ", last_layer.weight.shape, " core_weight: ", core_layer.weight.shape, " first_weight: ", first_layer.weight.shape)
+                        first_weight = first_layer.weight.data.squeeze(-1).squeeze(-1)
+                        core_weight = core_layer.weight.data
+                        last_weight = torch.transpose(last_layer.weight.data, 1, 0).squeeze(-1).squeeze(-1) 
+                        print("last_weight: ", last_weight.shape, " core_weight: ", core_weight.shape, " first_weight: ", first_weight.shape, )
+                        reconstructed_weight = tl.tucker_to_tensor(core_weight, [first_weight, last_weight])
 
-                    first_weight = first_layer.weight.data.squeeze(-1).squeeze(-1)
-                    core_weight = core_layer.weight.data
-                    last_weight = torch.transpose(last_layer.weight.data, 1, 0).squeeze(-1).squeeze(-1) 
-                    print("first_weight: ", first_weight.shape, " last_weight: ", last_weight.shape)
-                    reconstructed_weight = tl.tucker_to_tensor(core_weight, [first_weight, last_weight])
+                        assert(first_layer.bias is not None)
+                        assert(core_layer.bias is None)
+                        assert(last_layer.bias is None)
 
-                    assert(first_layer.bias is not None)
-                    assert(core_layer.bias is None)
-                    assert(last_layer.bias is None)
+                        reconstructed_bias = first_layer.bias.data
 
-                    reconstructed_bias = first_layer.bias.data
+                        reconstructed_layer = torch.nn.Conv2d(in_channels=first_layer.in_channels, \
+                                        out_channels=last_layer.out_channels, kernel_size=core_layer.kernel_size, stride=core_layer.stride,
+                                        padding=core_layer.padding, dilation=core_layer.dilation, bias=True)
+                        reconstructed_layer.weight.data = reconstructed_weight
+                        reconstructed_layer.bias.data = reconstructed_bias
 
-                    reconstructed_layer = torch.nn.Conv2d(in_channels=first_layer.in_channels, \
-                                    out_channels=last_layer.out_channels, kernel_size=core_layer.kernel_size, stride=core_layer.stride,
-                                    padding=core_layer.padding, dilation=core_layer.dilation, bias=True)
-                    reconstructed_layer.weight.data = reconstructed_weight
-                    reconstructed_layer.bias.data = reconstructed_bias
+                        model._modules[first_name] = reconstructed_layer
+                        model._modules[core_name] = torch.nn.Identity()
+                        model._modules[last_name] = torch.nn.Identity()
+                    elif(len(conv_layers_list) == 2):
+                        [core_layer, last_layer] = conv_layers_list
+                        [core_name, last_name] = conv_names_list 
+                        print("BEFORE last_weight: ", last_layer.weight.shape, " core_weight: ", core_layer.weight.shape)
+                        core_weight = core_layer.weight.data
+                        last_weight = last_layer.weight.data.squeeze(-1).squeeze(-1) #.contiguous() 
+                        #last_weight = last_weight.view(-1, last_weight.size(-1))
+                        print("last_weight: ", last_weight.shape, " core_weight: ", core_weight.shape)
+                        reconstructed_weight = tl.tucker_to_tensor(core_weight, [last_weight])
 
-                    model._modules[first_name] = reconstructed_layer
-                    model._modules[core_name] = torch.nn.Identity()
-                    model._modules[last_name] = torch.nn.Identity()
+                        assert(core_layer.bias is None)
+                        assert(last_layer.bias is not None)
+
+                        reconstructed_bias = last_layer.bias.data
+
+                        reconstructed_layer = torch.nn.Conv2d(in_channels=core_layer.in_channels, \
+                                        out_channels=last_layer.out_channels, kernel_size=core_layer.kernel_size, stride=core_layer.stride,
+                                        padding=core_layer.padding, dilation=core_layer.dilation, bias=True)
+                        reconstructed_layer.weight.data = reconstructed_weight
+                        reconstructed_layer.bias.data = reconstructed_bias
+
+                        model._modules[core_name] = torch.nn.Identity()
+                        model._modules[last_name] = reconstructed_layer
+                        
         else:
             item = next(iterator, None)
 
