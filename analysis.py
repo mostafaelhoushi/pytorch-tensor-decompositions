130c368d2d4f82ad1f1ec2cf09cfad738019e46b
Mostafa Elhoushi <m.elhoushi@ieee.org>
2020-01-22 00:12:33 -0500

    also show stats for decomposing fully trained model

--- analysis.py
+++ analysis.py
@@ -2,12 +2,14 @@ import torch
 import torchvision
 
 decomp_type = 'tucker'
-from_epochs = range(10,200,10)
+from_epochs = range(10,210,10)
 dataset = 'cifar10'
 arch = 'resnet56'
 
 for from_epoch in from_epochs:
-    model_dir = str('./models/' + dataset + '/' + arch + '/from_epoch_' + str(from_epoch) + '_' + decomp_type + '_decompose')
+    from_epoch_label = str('from_epoch_' + str(from_epoch) + '_') if from_epoch < 200 else ''
+
+    model_dir = str('./models/' + dataset + '/' + arch + '/' + from_epoch_label + decomp_type + '_decompose')
 
     model_file = str(model_dir + '/' + 'model.pth')
     checkpoint_file = str(model_dir + '/' + 'checkpoint.pth.tar')
@@ -16,8 +18,9 @@ for from_epoch in from_epochs:
     #model = torch.load(model_file)
     checkpoint = torch.load(checkpoint_file)
     state_dict = checkpoint['state_dict']
+    best_acc = checkpoint['best_acc1']
 
     num_params = sum(p.numel() for p in state_dict.values()) #len(model.parameters())
-    print(num_params)
+    print('#params: ', num_params, ' best_acc: ', best_acc)
 
     
